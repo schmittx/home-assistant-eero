9cd15af1e223897577c56869abcd17e36eb5d5d5
Thayer Fox <thayer@foxed.ca>
2023-05-03 18:21:32 -0700

    Refactor deprecated 'async_setup_platforms' method with 'async_forward_entry_setups'
    Refactor deprecated 'async_setup_platforms' method with 'async_forward_entry_setups' as per https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/eero/__init__.py
+++ custom_components/eero/__init__.py
@@ -346,7 +346,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             schema=SET_BLOCKED_APPS_SCHEMA,
         )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
